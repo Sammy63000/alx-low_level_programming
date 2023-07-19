4529331d2350639077cec26c95e55391733c6308
Samuel <sorpera639@gmail.com>
2023-07-19 10:23:18 -0700

    functions_nested_loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Adds two values
+ * @a: The first number to be computed
+ * @b: The second number to be computed
+ *
+ * Return: The sum of a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - Prints all natural numbers from n to 98
+ * @n: Starting number
+ *
+ * Description: This function prints all natural numbers from n to 98,
+ *              separated by commas and spaces.
+ */
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		for (; n > 98; n--)
+			printf("%d, ", n);
+	}
+	else
+	{
+		for (; n < 98; n++)
+			printf("%d, ", n);
+	}
+
+	printf("%d\n", n);
+}
